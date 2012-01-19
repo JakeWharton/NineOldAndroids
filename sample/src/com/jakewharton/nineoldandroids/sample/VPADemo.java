3bc72961b31afcd7188bb7ce5e1f803027127dfc
Jake Wharton <jakewharton@gmail.com>
2012-01-19 13:24:26 -0500

    Add Chet Haase's VPA demo. This shows some shortcomings in our implementation...

---
+++ sample/src/com/jakewharton/nineoldandroids/sample/VPADemo.java
@@ -0,0 +1,66 @@
+package com.jakewharton.nineoldandroids.sample;
+
+import com.nineoldandroids.view.ViewPropertyAnimator;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class VPADemo extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vpademo);
+
+        final LinearLayout container = (LinearLayout) findViewById(R.id.container);
+
+        final Button fadeOut = (Button) findViewById(R.id.fadeOut);
+        final Button fadeIn = (Button) findViewById(R.id.fadeIn);
+        final Button moveOver = (Button) findViewById(R.id.moveOver);
+        final Button moveBack = (Button) findViewById(R.id.moveBack);
+        final Button rotate = (Button) findViewById(R.id.rotate);
+        final Button animatingButton = (Button) findViewById(R.id.animatingButton);
+
+        // Set long default duration for the animator, for the purposes of this demo
+        ViewPropertyAnimator.animate(animatingButton).setDuration(2000);
+
+        fadeOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ViewPropertyAnimator.animate(animatingButton).alpha(0);
+            }
+        });
+
+        fadeIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ViewPropertyAnimator.animate(animatingButton).alpha(1);
+            }
+        });
+
+        moveOver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int xValue = container.getWidth() - animatingButton.getWidth();
+                int yValue = container.getHeight() - animatingButton.getHeight();
+                ViewPropertyAnimator.animate(animatingButton).x(xValue).y(yValue);
+            }
+        });
+
+        moveBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ViewPropertyAnimator.animate(animatingButton).x(0).y(0);
+            }
+        });
+
+        rotate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ViewPropertyAnimator.animate(animatingButton).rotationYBy(720);
+            }
+        });
+    }
+}
