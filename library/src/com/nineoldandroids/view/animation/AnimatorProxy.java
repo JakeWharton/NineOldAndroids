caf8155132ed1f99e542ff07a0d2b6dc5dbd449f
rotemmz <rotem@doit9.com>
2012-05-28 12:17:01 +0300

    Fixes a NullPointerException when an abject is being destroyed while animating.

--- library/src/com/nineoldandroids/view/animation/AnimatorProxy.java
+++ library/src/com/nineoldandroids/view/animation/AnimatorProxy.java
@@ -248,20 +248,24 @@ public final class AnimatorProxy extends Animation {
         }
     }
     private void invalidateAfterUpdate() {
-        View view = mView.get();
-        if (view == null) {
-            return;
-        }
+ 		View view = mView.get();
+		if (view == null)
+		{
+			return;
+		}
 
-        final RectF after = mAfter;
-        computeRect(after, view);
-        after.union(mBefore);
-
-        ((View)view.getParent()).invalidate(
-                (int) Math.floor(after.left),
-                (int) Math.floor(after.top),
-                (int) Math.ceil(after.right),
-                (int) Math.ceil(after.bottom));
+		final RectF after = mAfter;
+		computeRect(after, view);
+		after.union(mBefore);
+		View parent = (View) view.getParent();
+		
+		if (parent != null)
+		{
+			parent.invalidate((int) Math.floor(after.left), (int) Math.floor(after.top),
+					(int) Math.ceil(after.right), (int) Math.ceil(after.bottom));
+		}
+		else
+			view.invalidate();
     }
 
     private void computeRect(final RectF r, View view) {
